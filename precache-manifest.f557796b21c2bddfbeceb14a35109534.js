e7f763d1c028951cfcd0b68c801ff4708259cbbb
Goldsipa <goldsip@gmail.com>
2018-10-03 22:07:29 +0300

    Updates

---
+++ precache-manifest.f557796b21c2bddfbeceb14a35109534.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7aaa3277d8e868d3b02f",
+    "url": "/01-2-static-render-list/static/js/runtime~main.7aaa3277.js"
+  },
+  {
+    "revision": "ceb6f44d9f062fc48e32",
+    "url": "/01-2-static-render-list/static/js/main.ceb6f44d.chunk.js"
+  },
+  {
+    "revision": "9a498073e7355a684193",
+    "url": "/01-2-static-render-list/static/js/1.9a498073.chunk.js"
+  },
+  {
+    "revision": "ceb6f44d9f062fc48e32",
+    "url": "/01-2-static-render-list/static/css/main.7002b7d6.chunk.css"
+  },
+  {
+    "revision": "b2e42431f5c47431cd11740a23b65644",
+    "url": "/01-2-static-render-list/index.html"
+  }
+];
